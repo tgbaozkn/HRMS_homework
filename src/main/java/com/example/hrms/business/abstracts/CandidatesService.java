fa740aadc385a3bdd0ebece8408f08fe16e618cc
Tugba <39098806+tgbaozkn@users.noreply.github.com>
2021-05-31 00:50:00 +0300

    last

--- src/main/java/com/example/hrms/business/abstracts/CandidatesService.java
+++
@@ -1,8 +0,0 @@
-package com.example.hrms.business.abstracts;
-import java.util.*;
-
-import com.example.hrms.entities.concretes.Candidates;
-public interface CandidatesService {
-	List<Candidates> getAll();
-
-}
